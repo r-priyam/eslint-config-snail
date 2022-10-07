af2c48e5482bb3567c8a3b27ee42b2016ce67ab5
r-priyam <roy_priyam@outlook.com>
2022-10-07 10:26:51 +0530

    feat: add prettier-eslint-plugin

--- src/prettier.js
+++ src/prettier.js
@@ -1,9 +1,10 @@
 const prettier = require("eslint-config-prettier");
 
 module.exports = {
+	plugins: ["prettier"],
+	extends: ["plugin:prettier/recommended"],
 	rules: {
 		...prettier.rules,
-
 		"arrow-body-style": 0,
 		"line-comment-position": 0,
 		"no-inline-comments": 0,
