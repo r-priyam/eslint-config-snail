2af88cd81fa7d635ec7f94023da18977b633e6c1
r-priyam <roy_priyam@outlook.com>
2022-10-16 07:45:39 +0530

    chore: remove eslint-plugin-prettier

--- src/prettier.js
+++ src/prettier.js
@@ -1,8 +1,6 @@
 const prettier = require("eslint-config-prettier");
 
 module.exports = {
-	plugins: ["prettier"],
-	extends: ["plugin:prettier/recommended"],
 	rules: {
 		...prettier.rules,
 		"arrow-body-style": 0,
